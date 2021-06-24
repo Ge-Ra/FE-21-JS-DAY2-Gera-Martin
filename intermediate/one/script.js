6632842ddafe83ad6de14791f1f4189a23f05d2c
Martin Maier <martin.m@gmx.at>
2021-06-24 13:04:09 +0200

    inter one update

--- intermediate/one/script.js
+++ intermediate/one/script.js
@@ -1,12 +1,12 @@
 ((sentence) =>{
 
   let firstLetter = sentence.charAt(0).toUpperCase();
-  let slicedSentancePart1 = sentence.slice(1, 20);
-  console.log(firstLetter + slicedSentancePart1);
+  let slicedSentancePart = sentence.slice(1, 20);
+  console.log(firstLetter + slicedSentancePart);
    
 
   // let uppercase = sentence.charAt(0).toUpperCase();
   // console.log(uppercase + " " + sentence);
 
 
-})('my name is martin'); 
+})('i am a web developer'); 
