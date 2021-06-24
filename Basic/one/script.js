c13b7a7ecfb92ec89440c594252be72cdd6f7337
Martin Maier <martin.m@gmx.at>
2021-06-24 10:47:52 +0200

    basic one finished

---
+++ Basic/one/script.js
@@ -0,0 +1,7 @@
+function crystalGazer(jobTitle, location, partnersName, numberChildren){
+   return `You will be a ${jobTitle} in ${location} and married to ${partnersName} with ${numberChildren}.`
+
+}
+
+
+console.log(crystalGazer('Programmer', 'Vienna', 'Tom', 5));
